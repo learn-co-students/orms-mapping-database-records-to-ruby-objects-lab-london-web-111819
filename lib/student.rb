070f7127e43b192fb1b763d728983f4e418e659e
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-11-28 13:22:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,56 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students"
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}
+    # binding.pry
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
+  end
+
+  def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = 9"
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade <= 11"
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    sql = "SELECT * FROM students WHERE grade = 10"
+    request = DB[:conn].execute(sql).map{|row| new_from_db(row)}
+    request.slice(0, number)
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10"
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students where grade = #{grade}"
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}
   end
   
   def save
